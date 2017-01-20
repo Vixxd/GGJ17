6b7c46c887869d2f5192ccd9ccbd0ed799973dd1
Vicky <vicky@ivea.io>
2017-01-20 20:48:42 +0000

    Cloud and Sea Basic Parallax Test

---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour {
+
+	public float speed = 0;
+	
+	// Update is called once per frame
+	void Update () {
+
+		//As time elapses, shift the UV co-ordinates of this texture on this object
+		//This will make the texture repeat by default
+		gameObject.GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
+
+		
+	}
+}
