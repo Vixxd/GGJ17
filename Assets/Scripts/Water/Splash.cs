c890411a0467453fbb23bad6f3580d8b00be4e37
APWhiteside <awhiteside02@qub.ac.uk>
2017-01-22 13:19:07 +0000

    added wave particles

--- Assets/Scripts/Water/Splash.cs
+++ Assets/Scripts/Water/Splash.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class Splash : MonoBehaviour
 {
 
-    public ParticleSystem _ParticleSystem;
+    public GameObject _ParticleSystem;
+    public GameObject _start;
+    public GameObject _end;
+    public float LerpTime;
 
-	// Use this for initialization
-	void Start () {
+    private bool _palying;
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -17,6 +23,44 @@ public class Splash : MonoBehaviour
 	void Update () {
 		
 	}
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+ 
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Water"))
+        {
+            if (!_palying)
+            {
+                _palying = true;
+                StartCoroutine(moveParticleEffect());
+            }
+            _ParticleSystem.GetComponent<ParticleSystem>().Play();
+
+        }
+    }
+
+    private IEnumerator moveParticleEffect()
+    {
+        float timetotal = LerpTime;
+        float timeremaining = timetotal;
+        _ParticleSystem.GetComponent<ParticleSystem>().Play();
+
+        while (timeremaining > 0 && _palying)
+        {
+            timeremaining -= Time.deltaTime;
+
+            float t = (timeremaining / timetotal);
+            // Debug.Log("T: " + t + ", Time remaining :" + timeremaining);
+            _ParticleSystem.transform.position = Vector3.Lerp(_end.transform.position, _start.transform.position, t);
+            yield return null;
+
+        }
+        _ParticleSystem.GetComponent<ParticleSystem>().Stop();
+        _palying = false;
+    }
+
+
 
 
 }
