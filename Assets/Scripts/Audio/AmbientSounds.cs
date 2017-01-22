6caa25921b122516644849317b18b4e0de64f47e
APWhiteside <awhiteside02@qub.ac.uk>
2017-01-22 11:38:38 +0000

    started some sound stuck

---
+++ Assets/Scripts/Audio/AmbientSounds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSounds : MonoBehaviour {
+
+    public AudioSource AudioSource;
+
+    public AudioClip BackGroundSound, StartSound;
+
+    void OnEnable()
+    {
+        GameManager.Instance.OnGameStateChange += Instance_OnGameStateChange;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        AudioSource.clip = BackGroundSound;
+        AudioSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Instance_OnGameStateChange(GameEnums.GameState gameState)
+    {
+        if (gameState == GameEnums.GameState.Game)
+        {
+            AudioSource.PlayOneShot(StartSound); 
+        }
+    }
+}
